695397c86e89f0d59932e05acd1909d16d430514
mattbdennis-afk <mattbdennis@gmail.com>
2026-03-10 10:23:28 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from ‘react’
+import ReactDOM from ‘react-dom/client’
+import GolfTracker from ‘./golf_tracker_20260309_v8.jsx’
+
+ReactDOM.createRoot(document.getElementById(‘root’)).render(
+<React.StrictMode>
+<GolfTracker />
+</React.StrictMode>,
+)
