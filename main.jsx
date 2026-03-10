7ea645ea63070a658ca84152347817bd85f14e9b
mattbdennis-afk <mattbdennis@gmail.com>
2026-03-10 19:33:32 -0400

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import GolfTracker from "./golf tracker 20260309 v8.jsx"
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <GolfTracker />
-  </React.StrictMode>
-)
